6fc0e7bcfb7c7693f026a907ac231b9c37ff9507
vidura <vidurab@wso2.com>
2014-10-24 10:26:38 +0530

    Initializing git

---
+++ TestProject/src/TestPackage/Test.java
@@ -0,0 +1,14 @@
+package TestPackage;
+
+public class Test {
+
+	private int value1 = 0;
+	private int age = 0;
+	
+	public void setValue1(int value1){
+		this.value1 = value1;
+		if(value1 > 0){
+			System.out.println("fasjh");
+		}
+	}
+}
